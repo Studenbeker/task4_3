bcc669f64cb036d4ce15f6c5ba5587b8fa59d27a
Studenbeker <36299238+Studenbeker@users.noreply.github.com>
2018-04-01 12:53:57 +0300

    Create task4_3.sh

---
+++ task4_3.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+	ARGS=2
+	
+  dir="/tmp/backups/"
+	if [ ! -d "${dir}" ]; 
+  then
+	mkdir "${dir}"
+	fi 
+  
+  
+  if [ "$#" -ne 2 ]; 
+  then
+	echo "error: Illegal number of parameters" >&2;
+	echo "Specify a path to the folder and number of backups" >&2; 
+	exit 1
+	fi
+	
+  
+	if ! [[ -d $1 ]];
+  then
+	echo "error: $1 is a not a directory" >&2; 
+	exit 1
+	fi
+	
+  
+  num='^[0-9]+$'
+	if ! [[ $2 =~ $num ]] ;
+  then
+	echo "error: $2 Not a number" >&2; 
+	exit 1
+	fi
+	
+	
+	bdir="${1}"
+	bnum="$2"
+	bdname=$(echo "${1}" | sed 's/^[/]//' | sed -r 's/[/]/-/g')
+	file=${bname}-$(date '+%Y-%m-%d-%H%M%S').tar.gz
+	
+	
+	tar --create --gzip --file="$bdir$file" "${bdir}" 2> /dev/null
+	
+	
+	rm -f $(find "$bdir" -name "${bdname}*" -type f -printf "%Ts\t$bdir%P\n" | sort -n | head -n -"$2" | cut -f 2- )
+	
+	
+	exit 0
